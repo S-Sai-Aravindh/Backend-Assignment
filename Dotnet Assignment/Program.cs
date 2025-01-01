daa87ced47fd7e1953a734f026b8f27c9db7e1fb
Sai Aravindh <saiaravindhchangepond27@gmail.com>
2025-01-01 17:16:27 +0530

    01/01/2024 Tasks Completed

--- Dotnet Assignment/Program.cs
+++ Dotnet Assignment/Program.cs
@@ -6,9 +6,10 @@ namespace Assignment
 {
     internal class Program
     {
+        
         private static void Main(string[] args)
         {
-            //Console.WriteLine("Photo Book assignment")
+            //Console.WriteLine("Photo Book assignment");
             //PhotoBook p = new PhotoBook();
             //p.GetNumberPages();
 
@@ -19,22 +20,26 @@ namespace Assignment
             //b.GetNumberPages();
 
 
-            Console.WriteLine("Inheritance");
+            //Console.WriteLine("Inheritance");
 
-            Inheritance i = new Inheritance();
+            //Inheritance i = new Inheritance();
 
-            //Console.WriteLine(i.person[0]);
-            //Console.WriteLine(i.person[1]);
-            //Console.WriteLine(i.person[2]);
+            ////Console.WriteLine(i.person[0]);
+            ////Console.WriteLine(i.person[1]);
+            ////Console.WriteLine(i.person[2]);
 
-            Inheritance.Teacher t = new Inheritance.Teacher(i.person[0]);
-            Inheritance.Student s = new Inheritance.Student(i.person[1]);
-            Inheritance.Student s1 = new Inheritance.Student(i.person[2]);
+            //Inheritance.Teacher t = new Inheritance.Teacher(i.person[0]);
+            //Inheritance.Student s = new Inheritance.Student(i.person[1]);
+            //Inheritance.Student s1 = new Inheritance.Student(i.person[2]);
 
+            Console.Write("Enter the user name: ");
+            string name = Console.ReadLine();
+            OnlineAttendance o = new OnlineAttendance();
+            Banned b = new Banned();
 
-            Console.WriteLine("Shapes");
+            o.UserBanned += b.OnUserBanned;
 
-
+            o.getInput(name);
 
 
         }
--- Dotnet Assignment/obj/Debug/net8.0/Dotnet Assignment.AssemblyInfo.cs
+++ Dotnet Assignment/obj/Debug/net8.0/Dotnet Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3431869bd1ef6656428e58cffff487806306fd3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
